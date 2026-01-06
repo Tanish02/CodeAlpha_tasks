524f67f349f5afea53efd7a4ec54857a6987e76b
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2026-01-07 02:35:16 +0530

    add testing route

--- CodeAlpha_event-registration/Src/app.js
+++ CodeAlpha_event-registration/Src/app.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.use(express.json());
 
+// test route
+app.get("/", (req, res) => {
+  res.send("Event registration API route testing...");
+});
+
 // route
 app.post("/", (req, res) => {
   res.send("Event registration API running...");
