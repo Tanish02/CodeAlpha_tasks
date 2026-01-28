27f37a4ba48f70946c6540c6097e2c11cfb0b249
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2026-01-29 01:28:04 +0530

    add oder schema

---
+++ CodeAlpha_restaurant_management/src/schemas/order.schema.ts
@@ -0,0 +1,43 @@
+import { Schema, Types } from "mongoose";
+
+const OrderSchema = new Schema(
+  {
+    table: {
+      type: Types.ObjectId,
+      ref: "Table",
+      required: true,
+    },
+
+    items: [
+      {
+        menuItem: {
+          type: Types.ObjectId,
+          ref: "MenuItem",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+      },
+    ],
+
+    totalAmount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    status: {
+      type: String,
+      enum: ["pending", "preparing", "served", "cancelled"],
+      default: "pending",
+    },
+  },
+  { timestamps: true },
+);
+
+// end code
+
+export { OrderSchema };
