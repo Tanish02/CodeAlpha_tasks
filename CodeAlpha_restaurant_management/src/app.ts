bfeb032fc0a325d1d9e7ee714f3d41036389b3a4
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2026-01-30 14:11:01 +0530

    add menu routes integration in app and tested #Postman

--- CodeAlpha_restaurant_management/src/app.ts
+++ CodeAlpha_restaurant_management/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import menuRoutes from "./routes/menu.routes";
 
 const app = express();
 
@@ -9,6 +10,9 @@ app.use(express.json());
 app.get("/", (_req, res) => {
   res.send("Restaurant Management API running...");
 });
+
+// routes
+app.use("/api/menu", menuRoutes);
 
 export default app;
 
